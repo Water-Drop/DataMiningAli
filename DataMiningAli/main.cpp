984afc774c02216dcfbd50a904606f366375caf6
Water <mac@MactekiMacBook-Pro.local>
2014-04-03 14:39:24 +0800

    Initial Commit

---
+++ DataMiningAli/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DataMiningAli
+//
+//  Created by Water on 14-4-3.
+//  Copyright (c) 2014年 Water. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
